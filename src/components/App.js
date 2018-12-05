f5d94be798a15fdd62a77b0ed0d3496f1504719f
TrungDT <thanhtrung.it25@gmail.com>
2018-12-05 09:19:04 +0700

    Add ease-in transition when API is loaded

--- src/components/App.js
+++ src/components/App.js
@@ -51,39 +51,45 @@ class App extends Component {
     const { forecast, location, isLoaded } = this.state;
     const forecasts = forecast.simpleforecast;
 
+    const results = !isLoaded ? "result-loading" : "result-loaded";
+
     return (
       <div className="App">
-        <h1 className="title">4-Day Forecast</h1>
-        {isLoaded && (
-          <h3 className="location">
-            {location.city}, {location.state}, {location.country}
-          </h3>
-        )}
         {!isLoaded && <h3 className="loading">Loading...</h3>}
-        {isLoaded && (
-          <div className="weather-cards">
-            {forecasts.forecastday.map(data => (
-              <section key={data.period}>
-                <WeatherCard
-                  className="weather-card"
-                  tempUnit={this.state.tempUnit}
-                  isLoaded={this.state.isLoaded}
-                  icon={data.icon_url}
-                  high={data.high}
-                  low={data.low}
-                  day={data.date.weekday_short}
-                />
-              </section>
-            ))}
-          </div>
-        )}
+        <div className={results}>
+          {isLoaded && (
+            <div>
+              <h1 className="title">4-Day Forecast</h1>
+              <h3 className="location">
+                {location.city}, {location.state}, {location.country}
+              </h3>
+            </div>
+          )}
+          {isLoaded && (
+            <div className="weather-cards">
+              {forecasts.forecastday.map(data => (
+                <section key={data.period}>
+                  <WeatherCard
+                    className="weather-card"
+                    tempUnit={this.state.tempUnit}
+                    isLoaded={this.state.isLoaded}
+                    icon={data.icon_url}
+                    high={data.high}
+                    low={data.low}
+                    day={data.date.weekday_short}
+                  />
+                </section>
+              ))}
+            </div>
+          )}
 
-        <ConvertTempButton
-          className="convert-button"
-          toggleUnits={this.toggleUnits}
-          units={this.state.tempUnit}
-          isLoaded={this.state.isLoaded}
-        />
+          <ConvertTempButton
+            className="convert-button"
+            toggleUnits={this.toggleUnits}
+            units={this.state.tempUnit}
+            isLoaded={this.state.isLoaded}
+          />
+        </div>
       </div>
     );
   }
